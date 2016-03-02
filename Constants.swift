bda1621ce7e46747a085f8e189e3b7506f353924
Keaveney, Kevin <KKeavene@lndmactemp10.nms.dadcdigital.com>
2016-03-02 14:22:12 +0000

    Reachability added

--- Constants.swift
+++ Constants.swift
@@ -13,3 +13,9 @@ import Foundation
 typealias JSONDictionary = [String: AnyObject]
 
 typealias JSONArray = Array<AnyObject>
+
+let WIFI = "WIFI Available"
+
+let NOACCESS = "No Internet Available"
+
+let WWAN = "Cellular Access Available"
